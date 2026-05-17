1da650011fca5ec1a6b19c9922d852f4afb779ed
Nizaooooo <saladaapenas@gmail.com>
2026-05-17 02:16:24 -0300

    mudança da pagina home para responsividade e adaptação mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,24 +23,24 @@ function Home() {
       <Header />
       <Navbar />
 
-      <main className="font-['Oswald'] bg-[#F8F5F1] shadow-md w-full max-w-5xl mx-auto px-20 py-8">
-        <h1 className="bg-[#3B5C75] text-white text-4xl px-5 py-5 text-center mb-6">{saudacao}</h1>
+      <main className="font-['Oswald'] bg-[#F8F5F1] shadow-md w-full max-w-5xl mx-auto px-4 sm:px-10 md:px-20 py-8">
+        <h1 className="bg-[#3B5C75] text-white text-2xl sm:text-3xl md:text-4xl px-5 py-5 text-center mb-6">{saudacao}</h1>
 
         <section className="mb-6">
-          <p className="font-['Roboto'] text-2xl px-5 text-center">
+          <p className="font-['Roboto'] text-lg sm:text-xl md:text-2xl px-2 sm:px-5 text-center">
             Nossa plataforma disponibiliza fácil atendimento e consultas solidárias
             para pessoas que não têm condições de pagar de fato por um tratamento dentário.
           </p>
 
-          <h3 className="bg-[#3B5C75] rounded-full text-4xl text-center text-white py-2 my-4">👨‍⚕️ Clientes</h3>
-          <p className="font-['Roboto'] text-2xl px-5 text-center">
+          <h3 className="bg-[#3B5C75] rounded-full text-2xl sm:text-3xl md:text-4xl text-center text-white py-2 my-4">👨‍⚕️ Clientes</h3>
+          <p className="font-['Roboto'] text-lg sm:text-xl md:text-2xl px-2 sm:px-5 text-center">
             Nosso maior compromisso é com o bem-estar de cada pessoa que busca atendimento.
             Valorizamos o respeito, a empatia e a atenção em cada etapa do processo,
             oferecendo um cuidado humano e acessível.
           </p>
         </section>
 
-        <Link to="/fale-conosco" className="flex justify-center items-center mx-auto my-6 w-64 h-16 bg-[#3B5C75] text-[#F8F5F1] font-['Oswald'] text-xl rounded-xl no-underline hover:text-2xl transition-all">
+        <Link to="/fale-conosco" className="flex justify-center items-center mx-auto my-6 w-48 sm:w-56 md:w-64 h-14 md:h-16 bg-[#3B5C75] text-[#F8F5F1] font-['Oswald'] text-lg md:text-xl rounded-xl no-underline hover:text-2xl transition-all">
           FALE CONOSCO
         </Link>
 
