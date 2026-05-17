7a3a159613ea13ae02740c6af2a0d071a4e66dcc
Nizaooooo <saladaapenas@gmail.com>
2026-05-17 03:15:17 -0300

    arrumando pequenos erros para fazer o deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { VittaProvider } from './context/VittaContext.tsx'
+import { VittaProvider } from './context/Vittacontext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
