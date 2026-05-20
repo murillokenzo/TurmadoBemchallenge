dd24b209b2df80e093255cc825346256f4667df1
Nizaooooo <saladaapenas@gmail.com>
2026-05-19 22:26:58 -0300

    correção de problemas do vercel com import

--- src/pages/Agendamento.tsx
+++ src/pages/Agendamento.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import { useVitta } from '../context/VittaContext.tsx'
+import { useVitta } from '../context/VittaContext'
 import { postChamado } from '../services/api'
 
 type FormData = {
